97d41335d8bea770903d09d1a4bf4e5172a506fd
Chris Conlon <chris@wolfssl.com>
2022-07-29 14:10:29 -0600

    add test cases for Chacha.java

--- src/test/java/com/wolfssl/wolfcrypt/test/WolfCryptTestSuite.java
+++ src/test/java/com/wolfssl/wolfcrypt/test/WolfCryptTestSuite.java
@@ -26,9 +26,21 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AesTest.class, Des3Test.class, Md5Test.class, ShaTest.class,
-        Sha256Test.class, Sha384Test.class, Sha512Test.class, HmacTest.class,
-        RngTest.class, RsaTest.class, DhTest.class, EccTest.class })
+@SuiteClasses({
+        AesTest.class,
+        Des3Test.class,
+        ChachaTest.class,
+        Md5Test.class,
+        ShaTest.class,
+        Sha256Test.class,
+        Sha384Test.class,
+        Sha512Test.class,
+        HmacTest.class,
+        RngTest.class,
+        RsaTest.class,
+        DhTest.class,
+        EccTest.class
+        })
 public class WolfCryptTestSuite {
 
 }
