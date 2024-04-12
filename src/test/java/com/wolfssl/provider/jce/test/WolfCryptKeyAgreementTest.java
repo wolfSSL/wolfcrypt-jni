3198d3e8da4590eb8e237123668c7210a1379930
Chris Conlon <chris@wolfssl.com>
2024-04-12 16:34:23 -0600

    JNI: refactor DH threaded test to use AtomicIntegerArray

--- src/test/java/com/wolfssl/provider/jce/test/WolfCryptKeyAgreementTest.java
+++ src/test/java/com/wolfssl/provider/jce/test/WolfCryptKeyAgreementTest.java
@@ -33,7 +33,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 import javax.crypto.KeyAgreement;
