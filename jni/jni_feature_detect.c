6ccdf43175a5d77a33210ea6c1a3f84226e42173
Chris Conlon <chris@wolfssl.com>
2025-01-20 15:15:14 -0700

    JNI/JCE: update copyright to 2025

--- jni/jni_feature_detect.c
+++ jni/jni_feature_detect.c
@@ -1,6 +1,6 @@
 /* jni_feature_detect.c
  *
- * Copyright (C) 2006-2024 wolfSSL Inc.
+ * Copyright (C) 2006-2025 wolfSSL Inc.
  *
  * This file is part of wolfSSL.
  *
--- jni/jni_wolfssl_cert_manager.c
+++ jni/jni_wolfssl_cert_manager.c
@@ -1,6 +1,6 @@
 /* jni_wolfssl_cert_manager.c
  *
- * Copyright (C) 2006-2024 wolfSSL Inc.
+ * Copyright (C) 2006-2025 wolfSSL Inc.
  *
  * This file is part of wolfSSL.
  *
