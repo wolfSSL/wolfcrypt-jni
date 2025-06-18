56e15d5fde0ce5469dcc0881be3fbc562f2a01db
Chris Conlon <chris@wolfssl.com>
2025-06-18 13:51:13 -0600

    JCE example: add KeyStore benchmark example app

---
+++ examples/provider/KeyStoreBenchmark.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ./examples/build/provider
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../lib:/usr/local/lib
+java -classpath ../../../lib/wolfcrypt-jni.jar:./ -Dsun.boot.library.path=../../../lib/ KeyStoreBenchmark $@
