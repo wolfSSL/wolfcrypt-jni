8baf896daac77d1ca804ef165e943ec8f0886c27
Chris Conlon <chris@wolfssl.com>
2026-01-08 10:12:29 -0700

    Update copyright dates to 2026

--- src/main/java/com/wolfssl/provider/jce/WolfCryptProvider.java
+++ src/main/java/com/wolfssl/provider/jce/WolfCryptProvider.java
@@ -1,6 +1,6 @@
 /* wolfCryptProvider.java
  *
- * Copyright (C) 2006-2025 wolfSSL Inc.
+ * Copyright (C) 2006-2026 wolfSSL Inc.
  *
  * This file is part of wolfSSL.
  *
--- src/test/java/com/wolfssl/wolfcrypt/test/EccTest.java
+++ src/test/java/com/wolfssl/wolfcrypt/test/EccTest.java
@@ -1,6 +1,6 @@
 /* EccTest.java
  *
- * Copyright (C) 2006-2025 wolfSSL Inc.
+ * Copyright (C) 2006-2026 wolfSSL Inc.
  *
  * This file is part of wolfSSL.
  *
