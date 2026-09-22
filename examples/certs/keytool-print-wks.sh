709b6be7915360c64574d31dc9c4e652a674cf23
Chris Conlon <chris@wolfssl.com>
2026-09-22 16:37:02 -0600

    F-13005: anchor the WKS keytool print script to its own directory and quote its arguments

--- examples/certs/keytool-print-wks.sh
+++ examples/certs/keytool-print-wks.sh
@@ -1,18 +1,24 @@
-
+#!/bin/bash
 # Script to print out WKS keystores using keytool -list
 #
 # Primarily used as a sanity check that keytool can successfully process
 # WKS KeyStore files using the -list command
 #
-# Export library paths for Linux and Mac to find shared JNI library
-export LD_LIBRARY_PATH=../../lib:$LD_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=../../lib:$DYLD_LIBRARY_PATH
+# Paths anchored to this script location, runs from any directory
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd) || exit 1
+LIB_DIR="$SCRIPT_DIR/../../lib"
 
-# ARGS: <keystore file> <password>
+# Export library paths for Linux and Mac to find shared JNI library
+export LD_LIBRARY_PATH="$LIB_DIR${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export DYLD_LIBRARY_PATH="$LIB_DIR${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}"
+
+# ARGS: <keystore file name in this directory> <password>
 print_wks() {
     printf "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
-    printf "KEYSTORE: $1\n"
-    keytool -list -provider com.wolfssl.provider.jce.WolfCryptProvider --providerpath ../../lib/wolfcrypt-jni.jar -storetype WKS -storepass "$2" -keystore ${1}
+    printf "KEYSTORE: %s\n" "$1"
+    keytool -list -provider com.wolfssl.provider.jce.WolfCryptProvider \
+        --providerpath "$LIB_DIR/wolfcrypt-jni.jar" -storetype WKS \
+        -storepass "$2" -keystore "$SCRIPT_DIR/$1"
     if [ $? -ne 0 ]; then
         printf "fail"
         exit 1
